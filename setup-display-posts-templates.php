a0dcd0a8e15e9a26877101963dfc59de2c633674
Jake Almeda <jakealmeda@gmail.com>
2020-04-21 16:46:58 +0800

    Featured Category and Image Placeholder
    Added an option to show a featured category (for now this is static, meaning it is manually assigned). Also added a default image when no images are associated to the post entry but images are called in the shortcode

--- setup-display-posts-templates.php
+++ setup-display-posts-templates.php
@@ -16,27 +16,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 function be_dps_template_part( $output, $original_atts ) {
     
-	// Return early if our "layout" attribute is not specified
-	if( empty( $original_atts['layout'] ) )
-		return $output;
+    // Return early if our "layout" attribute is not specified
+    if( empty( $original_atts['layout'] ) )
+        return $output;
 
-	ob_start();
+    ob_start();
 
-	// The line below is Bill's code which I can't make it to work
-	//get_template_part( get_stylesheet_directory_uri().'/partials/dps/', 'dps-'.$original_atts[ "layout" ].'.php' );
+    // The line below is Bill's code which I can't make it to work
+    //get_template_part( get_stylesheet_directory_uri().'/partials/dps/', 'dps-'.$original_atts[ "layout" ].'.php' );
 
-	// The next line is my code which gets templates from the THEME file:
-	//include get_stylesheet_directory().'/partials/dps/dps-'.$original_atts[ "layout" ].'.php';
+    // The next line is my code which gets templates from the THEME file:
+    //include get_stylesheet_directory().'/partials/dps/dps-'.$original_atts[ "layout" ].'.php';
 
-	// The next line is my code which gets templates from this plugin directory:
-	include plugin_dir_path( __FILE__ ).'templates/'.$original_atts[ "layout" ].'.php';
+    // The next line is my code which gets templates from this plugin directory:
+    include plugin_dir_path( __FILE__ ).'templates/'.$original_atts[ "layout" ].'.php';
 
-	$new_output = ob_get_clean();
-	
-	if( !empty( $new_output ) )
-		$output = $new_output;
-	
-	return $output;
+    $new_output = ob_get_clean();
+    
+    if( !empty( $new_output ) )
+        $output = $new_output;
+    
+    return $output;
 
 }
 add_action( 'display_posts_shortcode_output', 'be_dps_template_part', 10, 2 );
@@ -45,7 +45,7 @@ add_action( 'display_posts_shortcode_output', 'be_dps_template_part', 10, 2 );
 // HANDLE IMAGES TO BE DISPLAYED
 if( !function_exists( 'setup_show_images' ) ) {
     
-    function setup_show_images( $images, $pid ) {
+    function setup_show_images( $images, $pid, $img_atts = FALSE ) {
         
         foreach( $images as $key => $value ) {
             
@@ -54,6 +54,7 @@ if( !function_exists( 'setup_show_images' ) ) {
              * $value  --> this is the image size
              */
             
+            // Check what image to display
             if( $key == 'featured' ) {
                 
                 // featured image
@@ -65,13 +66,32 @@ if( !function_exists( 'setup_show_images' ) ) {
                 $out .= wp_get_attachment_image( get_post_meta( $pid, $key, TRUE ), $value );
                 
             }
-            
+
             // break if $out has content
-            if( $out ) break;
+            if( $out ) {
+
+                // validate link
+                if( $img_atts[ 'permalink' ] ) {
+
+                    // determine where to open the link
+                    if( $img_atts[ 'target' ] )
+
+                    $out = '<a href="'.get_the_permalink( $pid ).'">'.$out.'</a>';
+
+                }                
+
+                // exit loop if an image is confirmed
+                break;
+            }
             
         }
         
-        return $out;
+        // validate if there's an image, else, show placeholder image
+        if( $out ) {
+            return $out;
+        } else {
+            return '<img src="'.get_stylesheet_directory_uri().'/assets/images/brand-smarterwebpackages.png" />';
+        }
         
     }
     
